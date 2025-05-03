90b4917d6c27c7ac2287e57c52d458631412697c
TwilCynder <twilcynder@gmail.com>
2025-05-03 19:20:50 +0200

    harmonizing with upstream

--- include/globals.js
+++ include/globals.js
@@ -264,6 +264,7 @@ async function LoadSettings() {
   }
 
   tsh_settings = _.defaultsDeep(file_settings, global_settings);
+  console.log("tsh_settings : ", tsh_settings);
 }
 
 // Registers element for content fitting inside div if the div is resized
--- scoreboard/index.js
+++ scoreboard/index.js
@@ -164,7 +164,6 @@ LoadEverything().then(() => {
       ].entries()) {
         for (const [p, player] of [team.player["1"]].entries()) {
           if (player) {
-            console.log("TTTT" , await Transcript(player.name))
             SetInnerHtml(
               $(`.p${t + 1}.container .name`),
               `
