b9b5e7e87034108b561d588e1d5fda3e82302c77
TwilCynder <twilcynder@gmail.com>
2025-07-28 06:00:06 +0200

    team roster layout "finished" (ugly ?)

--- team_rosterTLS/index.js
+++ team_rosterTLS/index.js
@@ -1,7 +1,16 @@
+const default_config = {
+  display : {
+    "inline_pronoun" : true,
+    "sponsor":  true
+  }
+}
+
 LoadEverything().then(() => {
   if (!window.PLAYER) {
     window.PLAYER = 1;
   }
+
+  let config = _.defaultsDeep(window.config, default_config);
 
   gsap.config({ nullTargetWarn: false, trialWarn: false });
 
@@ -25,7 +34,18 @@ LoadEverything().then(() => {
         .forEach( async(_, p) => {
           team_html += `
           <div class="player${p + 1} player_container">
-            ${config.display.sponsor ? `
+
+          </div>`;
+        });
+      $(".content").html(team_html);
+
+      startingAnimation = gsap.timeline({paused: false})
+      for (const [p, player] of Object.entries(data.score[window.scoreboardNumber].team[window.PLAYER].player)) {
+        SetInnerHtml(
+          $(
+            `.player${p}`
+          ),
+          `${config.display.sponsor ? `
             <span class="sponsor">
               ${player.team ? player.team : ""}
               ${config.teamNameSeparator ? config.teamNameSeparator : ""}
@@ -38,52 +58,16 @@ LoadEverything().then(() => {
             ${player.pronoun ? player.pronoun : ""}
             </span>`
             : ""
-          }
-          </div>`;
-        });
-      $(".player1_content").html(tournament_html);
+          }`
+        );
 
-      startingAnimation = gsap.timeline({paused: false})
-      for (const [s, tournament] of Object.values(
-        data.score[window.scoreboardNumber].history_sets[window.PLAYER]
-      )
-        .slice(0, 6)
-        .entries()) {
-        SetInnerHtml(
-          $(
-            `.player1_content .tournament${
-              s + 1
-            } .info .tournament_info .tournament_name`
-          ),
-          tournament.tournament_name
-        );
-        SetInnerHtml(
-          $(
-            `.player1_content .tournament${
-              s + 1
-            } .info .tournament_info .event_name`
-          ),
-          tournament.event_name
-        );
-        SetInnerHtml(
-          $(`.player1_content .tournament${s + 1} .info .tournament_logo`),
-          `
-              <span class="logo" style="background-image: url('${tournament.tournament_picture}')"></span>
-            `
-        );
-        SetInnerHtml(
-          $(`.player1_content .tournament${s + 1} .info .placement`),
-          tournament.placement +
-            `<span class="ordinal">${getNumberOrdinal(
-              tournament.placement
-            )}</span><span class="num_entrants">/${tournament.entrants}</span>`
-        );
         startingAnimation.from(
-          $(`.tournament${s + 1}`),
+          $(`.player${p + 1}`),
           { x: -100, autoAlpha: 0, duration: 0.3 },
-          0.2 + 0.2 * s
+          0.2 + 0.2 * p
         );
       }
+
       console.log("restart animation")
       startingAnimation.restart();
     }
