322ac05d9650a380f3cb69a68e88d0b663fbb2ad
TwilCynder <twilcynder@gmail.com>
2026-07-27 21:09:51 +0200

    casters overlay

--- castersTLS_variable/index.js
+++ castersTLS_variable/index.js
@@ -7,75 +7,96 @@ LoadEverything(() => {
         };
     }
     
-    Start = async (event) => {};
+    gsap.config({ nullTargetWarn: false, trialWarn: false });
+    
+    let startingAnimation = gsap
+    .timeline({ paused: true })
+    .from([".logo"], { duration: 0.5, autoAlpha: 0, ease: "power2.inOut" }, 0.5)
+    .from(
+        [".fade"],
+        {
+            duration: 0.8,
+            autoAlpha: 0,
+            ease: "power2.out",
+        },
+        0
+    )
+    
+    Start = async (event) => {
+        startingAnimation.restart();
+    };
     
     Update = async (event) => {
         let data = event.data;
         let oldData = event.oldData;
         
+        const commentators = Object.values(data.commentary).filter(commentator => !!commentator.name);
+        const oldCommsLength = oldData.commentary ? Object.values(oldData.commentary).filter(commentator => !!commentator.name).length : 0;
+        
         if (
-            Object.keys(oldData).length == 0 ||
-            Object.keys(oldData.commentary).length !=
-            Object.keys(data.commentary).length
-        ) {
-            let html = "";
+            commentators.length !=
+            oldCommsLength
+        ) { 
+            const commentators = Object.values(data.commentary).filter(commentator => !!commentator.name);
             
-            const commentators = Object.values(data.commentary);
-            if (commentators.length == 2){
-                html = `
-                    <div class="commentators_outer_container">
-                        <img class="absol bg" src="Assets/common/Overlay">
+            let castersDivs = "", globalClasss = null;
+            
+            if (commentators.length == 2 && false){
+                castersDivs = `
+                    <div class="commentator c1">
+                        <div class="name"></div>
+                        <div class="twitter"></div>
+                    </div>
+                    <div class="commentator c2">
+                        <div class="name"></div>
+                        <div class="twitter"></div>
                     </div>
                 `
-            }
+                globalClasss = "duo bg";
+            } else {
+                for (let i = 0; i < commentators.length; i++){
+                    castersDivs += `
+                        <div class="commentator c${i + 1}">
+                            <img class="caster_bg" src="../../../Assets/common/Overlay/CASTER_SOLO_2.png"/>
 
-            Object.values(data.commentary).forEach((commentator, index) => {
-                html += `
-              <div class="commentator_container commentator${index}">
-                  <div class="name"></div>
-                  <div class="pronoun"></div>
-                  ${
-                window.config.size == "normal"
-                ? `<div class="real_name"></div>`
-                : ""
+                            <div class="name"></div>
+                            <div class="twitter"></div>
+                        </div>
+                    `
+                }
+                globalClasss = "solo";
             }
-                  ${
-            window.config.size == "normal" ||
-            window.config.size == "mini"
-            ? `<div class="twitter"></div>`
-            : ""
-        }
-              </div>
-          `;
-    });
-    $(".container").html(html);
-}
-
-for (const [index, commentator] of casters.entries()) {
-    if (commentator.name) {
-        $(`.commentator${index}`).css("display", "");
-        SetInnerHtml(
-            $(`.commentator${index} .name`),
+            
+            const html = `
+                <div class="commentators_outer_container ${globalClasss}">
+                    ${castersDivs}
+                </div>
             `
-            ${/*<span class="mic_icon"></span>*/""}
-            <span class="team">
-              ${commentator.team ? commentator.team + "&nbsp;" : ""}
-            </span>
-            ${await Transcript(commentator.name)}
-          `
-        );
-        SetInnerHtml($(`.commentator${index} .pronoun`), commentator.pronoun);
-        SetInnerHtml(
-            $(`.commentator${index} .real_name`),
-            commentator.real_name
-        );
-        SetInnerHtml(
-            $(`.commentator${index} .twitter`),
-            commentator.twitter ? "@" + commentator.twitter : ""
-        );
-    } else {
-        $(`.commentator${index}`).css("display", "none");
-    }
-}
-};
-});
+            $(".content").html(html);
+        }
+        
+        for (const [index, commentator] of Object.entries(data.commentary)) {
+            if (commentator.name) {
+                $(`.c${index}`).css("display", "");
+                SetInnerHtml(
+                    $(`.c${index} .name`),
+                    `
+                        <span class="team">
+                            ${commentator.team || ""}
+                        </span>
+                        ${await Transcript(commentator.name)}
+                        <span class="pronoun scoreboard_pronoun">
+                            ${commentator.pronoun || ""}
+                        </span>
+                `);
+                    SetInnerHtml(
+                        $(`.c${index} .twitter`),
+                        commentator.twitter ? "@" + commentator.twitter : ""
+                    );
+                } else {
+                    $(`.c${index}`).css("display", "none");
+                }
+            }
+        };
+    });
+    
